21e2ac61a1dd7b486677695bcb5869d2786b1826
testing <anmbo@hotmail.com>
2025-07-29 20:46:14 -0400

    Add project files.

---
+++ WebAppliNaissance/Models/Declarant.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppliNaissance.Models
+{
+    public class Declarant
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Nom")]
+        public string Nom { get; set; }
+
+        [Required]
+        [Display(Name = "Prénom")]
+        public string Prenom { get; set; }
+
+        [Required]
+        [Display(Name = "Adresse")]
+        public string Adresse { get; set; }
+
+        [Required]
+        [Display(Name = "CNI")]
+        public string CNI { get; set; }
+
+        [Required]
+        [Display(Name = "Lien avec l'enfant")]
+        public string LienAvecEnfant { get; set; }
+    }
+}
---
+++ WebAppliNaissance/Models/Naissance.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppliNaissance.Models
+{
+    public class Naissance
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Nom de l'enfant")]
+        public string NomEnfant { get; set; }
+
+        [Required]
+        [Display(Name = "Prénom de l'enfant")]
+        public string PrenomEnfant { get; set; }
+
+        [Required]
+        [Display(Name = "Date de naissance")]
+        [DataType(DataType.Date)]
+        public DateTime DateNaissance { get; set; }
+
+        [Required]
+        [Display(Name = "Heure de naissance")]
+        [DataType(DataType.Time)]
+        public DateTime HeureNaissance { get; set; }
+
+        [Required]
+        [Display(Name = "Lieu de naissance")]
+        public string LieuNaissance { get; set; }
+
+        [Required]
+        [Display(Name = "Sexe")]
+        public string Sexe { get; set; }
+
+        [Required]
+        [Display(Name = "Nom du père")]
+        public string NomPere { get; set; }
+
+        [Required]
+        [Display(Name = "Prénom du père")]
+        public string PrenomPere { get; set; }
+
+        [Required]
+        [Display(Name = "Nom de la mère")]
+        public string NomMere { get; set; }
+
+        [Required]
+        [Display(Name = "Prénom de la mère")]
+        public string PrenomMere { get; set; }
+
+        [Display(Name = "Date d'enregistrement")]
+        public DateTime DateEnregistrement { get; set; } = DateTime.Now;
+
+        [Display(Name = "Numéro d'acte")]
+        public string NumeroActe { get; set; }
+
+        public int DeclarantId { get; set; }
+        public Declarant Declarant { get; set; }
+    }
+}
